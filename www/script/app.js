3f0a7694b332b7d7c4da7896ce8045d744b91111
JanMod <jan-moder@hotmail.de>
2018-02-08 20:22:42 +0100

    generate electron

--- www/script/app.js
+++ www/script/app.js
@@ -1,2 +1,20 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngMaterial']);
 console.log(app);
+/*
+app.config(function($mdIconProvider) {
+
+    // Register a default set of SVG icon definitions
+    $mdIconProvider.defaultIconSet('my/app/icons.svg')
+  
+  })
+  .run(function($templateRequest){
+  
+    // Pre-fetch icons sources by URL and cache in the $templateCache...
+    // subsequent $templateRequest calls will look there first.
+  
+    var urls = [ 'imy/app/icons.svg', 'img/icons/android.svg'];
+  
+    angular.forEach(urls, function(url) {
+      $templateRequest(url);
+    });
+    */
